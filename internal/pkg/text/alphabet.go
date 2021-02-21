d6c1e75da571e9cc4856f987d8a66f73865be80a
revett <2796074+revett@users.noreply.github.com>
2021-02-21 21:39:46 +0000

    Export fields

--- internal/pkg/text/alphabet.go
+++ internal/pkg/text/alphabet.go
@@ -16,14 +16,14 @@ const EnglishAlphabet = "abcdefghijklmnopqrstuvwxyz"
 
 // Alphabet contains a slice of letters.
 type Alphabet struct {
-	combinations []string
-	letters      []string
+	Combinations []string
+	Letters      []string
 }
 
 // NewAlphabet creates and configures an Alphabet struct.
 func NewAlphabet(s string, opts ...func(*Alphabet)) *Alphabet {
 	a := &Alphabet{
-		letters: strings.Split(s, ""),
+		Letters: strings.Split(s, ""),
 	}
 
 	for _, o := range opts {
@@ -39,27 +39,27 @@ func NewAlphabet(s string, opts ...func(*Alphabet)) *Alphabet {
 func WithMixedCase() func(*Alphabet) {
 	return func(a *Alphabet) {
 		var mc []string
-		for _, l := range a.letters {
+		for _, l := range a.Letters {
 			mc = append(mc, strings.ToLower(l), strings.ToUpper(l))
 		}
 
-		a.letters = mc
+		a.Letters = mc
 	}
 }
 
 // RandomLetterPair returns a random letter pair from the configured alphabet.
 func (a Alphabet) RandomLetterPair() string {
-	return a.combinations[rand.Intn(len(a.combinations))]
+	return a.Combinations[rand.Intn(len(a.Combinations))]
 }
 
 func (a *Alphabet) generateCombinations() {
 	var c []string
 
-	for _, lx := range a.letters {
-		for _, ly := range a.letters {
+	for _, lx := range a.Letters {
+		for _, ly := range a.Letters {
 			c = append(c, fmt.Sprintf("%s%s", lx, ly))
 		}
 	}
 
-	a.combinations = c
+	a.Combinations = c
 }
