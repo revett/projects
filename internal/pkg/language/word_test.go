2cb91094fad4c648b4bda08e6888c97f7f7af940
revett <2796074+revett@users.noreply.github.com>
2021-02-22 00:28:25 +0000

    Working tests

--- internal/pkg/language/word_test.go
+++ internal/pkg/language/word_test.go
@@ -16,11 +16,55 @@ func TestWordConsonantVowelPattern(t *testing.T) {
 			input: "cat",
 			want:  "CVC",
 		},
+		"ClosedShort": {
+			input: "at",
+			want:  "VC",
+		},
+		"Open": {
+			input: "me",
+			want:  "CV",
+		},
+		"VowelConsonantE": {
+			input: "made",
+			want:  "CVCE",
+		},
+		"FalseVowelConsonantE": {
+			input: "abductee",
+			want:  "VCCVCCVV",
+		},
+		"RControlled": {
+			input: "her",
+			want:  "CVR",
+		},
+		"RControlledNested": {
+			input: "bird",
+			want:  "CVRC",
+		},
+		"Diphthong": {
+			input: "meet",
+			want:  "CVVC",
+		},
+		"ConsonantLE": {
+			input: "table",
+			want:  "CVCLE",
+		},
+		"FalseConsonantLE": {
+			input: "tale",
+			want:  "CVCC",
+		},
+		"UpperCase": {
+			input: "FOOD",
+			want:  "CVVC",
+		},
+		"MixedCase": {
+			input: "Golang",
+			want:  "CVCVCC",
+		},
 	}
 
 	for n, test := range tests {
 		t.Run(n, func(subtest *testing.T) {
-			w := language.Word("foo")
+			w := language.NewWord(test.input)
 			assert.Equal(t, test.want, w.ConsonantVowelPattern())
 		})
 	}
