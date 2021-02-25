79b7e5868df481379e17d066f018771c52ba8054
revett <2796074+revett@users.noreply.github.com>
2021-02-25 00:38:40 +0000

    Use NPM

--- web/static/src/js/chess.js
+++ web/static/src/js/chess.js
@@ -1,19 +1,17 @@
-import chessboard from "./chessboard-1.0.0.js"
+import chessboard from 'chessboardjs'
 
 const onBoardChange = (o, n) => {
   const fen = document.querySelector('code')
-  fen.innerHTML = Chessboard.objToFen(n)
+  fen.innerHTML = chessboard.objToFen(n)
 }
 
 let opts = {
   onChange: onBoardChange,
 }
-const b = Chessboard('b', opts)
+const b = chessboard('b', opts)
 b.start(true)
 
 const newFen = (e) => {
-  console.log(e)
-  console.log(e.target.value)
   if (e.code === 'Enter') {
     b.position(e.target.value, true)
     e.target.value = ''
