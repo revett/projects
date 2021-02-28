a5fe63f838911bb82f11a90ff0aabc93ef71f885
Charlie Revett <2796074+revett@users.noreply.github.com>
2021-02-28 16:30:48 +0000

    Github Actions Not Triggered (#12)

--- cmd/uci-engine-wrapper/main.go
+++ cmd/uci-engine-wrapper/main.go
@@ -15,14 +15,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	e.SetOptions(uci.Options{
+	err = e.SetOptions(uci.Options{
 		Hash:    128,
 		Ponder:  false,
 		OwnBook: true,
 		MultiPV: 4,
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	e.SetFEN("rnb4r/ppp1k1pp/3bp3/1N3p2/1P2n3/P3BN2/2P1PPPP/R3KB1R b KQ - 4 11")
+	err = e.SetFEN("rnb4r/ppp1k1pp/3bp3/1N3p2/1P2n3/P3BN2/2P1PPPP/R3KB1R b KQ - 4 11")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	opts := uci.HighestDepthOnly | uci.IncludeUpperbounds | uci.IncludeLowerbounds
 	r, err := e.GoDepth(10, opts)
