7e646bae9c1d8ed60b55066dcdc13fb0effd28e5
revett <2796074+revett@users.noreply.github.com>
2021-03-01 00:29:30 +0000

    Integrate stockfish

--- api/index.go
+++ api/index.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 
+	"github.com/freeeve/uci"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/revett/projects/internal/uci-engine-wrapper/handlers"
@@ -24,25 +23,34 @@ func main() {
 }
 
 // Handler is required by Vercel.
-func Handler(w http.ResponseWriter, r *http.Request) {
-	path, err := os.Getwd()
+func Handler(w http.ResponseWriter, req *http.Request) {
+	e, err := uci.NewEngine("/var/task/templates/stockfish_13_linux_x64_bmi2")
 	if err != nil {
-			log.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println(path)
 
-	var files []string
-
-	root := path
-	err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-			files = append(files, path)
-			return nil
-	})
+	engOpts := uci.Options{
+		Hash:    1024,
+		MultiPV: 1,
+		OwnBook: true,
+		Threads: 1,
+	}
+	err = e.SetOptions(engOpts)
 	if err != nil {
-			log.Println(err)
+		log.Fatal(err)
 	}
-	for _, file := range files {
-			fmt.Println(file)
+
+	err = e.SetFEN("r1bqkb1r/pppp1ppp/2n2n2/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 0 1")
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	r, err := e.Go(0, "", 5000)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(r)
+
   fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
