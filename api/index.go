1da4c28d920f1ac34c85d26b7c25c72fbcd3a7fc
revett <2796074+revett@users.noreply.github.com>
2021-03-14 18:04:54 +0000

    Update vercel.json

--- api/index.go
+++ api/index.go
@@ -4,16 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/revett/projects/pkg/uci"
 )
 
 // Handler is the exported http.HandlerFunc for Vercel.
 func Handler(w http.ResponseWriter, r *http.Request) {
-	s := r.URL.Query().Get("binary")
-	path := fmt.Sprintf("/var/task/engines/%s", s)
-
-	e, err := uci.NewEngine(path, uci.LogOutput)
+	e, err := uci.NewEngine(os.Getenv("STOCKFISH_PATH"), uci.LogOutput)
 	if err != nil {
 		log.Println(err)
 		return
@@ -24,7 +22,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		uci.UCINewGameCommand(),
 		uci.IsReadyCommand(),
 		uci.PositionCommand(uci.WithFEN("r3kb1r/pp1q1ppp/4p3/8/3P4/8/P1P2PPP/R1BQ1RK1 b kq - 1 12")),
-		uci.GoCommand(uci.WithDepth(5)),
+		uci.GoCommand(uci.WithDepth(10)),
 	)
 	if err != nil {
 		log.Println(err)
