433b82428a6a4e4398fb3e3426670ad153a164be
revett <2796074+revett@users.noreply.github.com>
2021-03-14 17:39:13 +0000

    Use .Go cmd

--- api/index.go
+++ api/index.go
@@ -23,6 +23,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		uci.UCINewGameCommand(),
 		uci.IsReadyCommand(),
 		uci.PositionCommand(uci.WithFEN("r3kb1r/pp1q1ppp/4p3/8/3P4/8/P1P2PPP/R1BQ1RK1 b kq - 1 12")),
+		uci.GoCommand(uci.WithDepth(5)),
 	)
 	if err != nil {
 		log.Println(err)
