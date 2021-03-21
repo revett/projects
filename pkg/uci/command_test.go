d5e92aafcec944806e5a9209f0eb41a327d15815
revett <2796074+revett@users.noreply.github.com>
2021-03-21 17:18:09 +0000

    TestUCICommand

--- pkg/uci/command_test.go
+++ pkg/uci/command_test.go
@@ -25,3 +25,28 @@ func TestIsReadyCommand(t *testing.T) {
 	err = e.Close()
 	assert.NoError(t, err)
 }
+
+func TestUCICommand(t *testing.T) {
+	mc := mockCommander{
+		out: []string{
+			"id name Stockfish 13",
+			"id author the Stockfish developers (see AUTHORS file)",
+			"",
+			"option name Debug Log File type string default",
+			"option name Contempt type spin default 24 min -100 max 100",
+			"option name Threads type spin default 1 min 1 max 512",
+			"uciok",
+		},
+	}
+
+	e, err := uci.NewEngine(mc.Command, mockEnginePath)
+	assert.NoError(t, err)
+
+	err = e.Run(
+		uci.UCICommand(),
+	)
+	assert.NoError(t, err)
+
+	err = e.Close()
+	assert.NoError(t, err)
+}
