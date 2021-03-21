dda1a148cb572d6407f2d3f4c85901ca228c6e20
revett <2796074+revett@users.noreply.github.com>
2021-03-21 17:26:16 +0000

    TestGoCommand

--- pkg/uci/command_test.go
+++ pkg/uci/command_test.go
@@ -1,11 +1,36 @@
 package uci_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/revett/projects/pkg/uci"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestGoCommand(t *testing.T) {
+	bestMove := "d2d4"
+	mc := mockCommander{
+		out: []string{
+			"info string NNUE evaluation using nn-62ef826d1a6d.nnue enabled",
+			"info depth 1 seldepth 1 multipv 1 score cp 29 nodes 20 nps 20000 tbhits 0 time 1 pv d2d4",
+			"info depth 2 seldepth 2 multipv 1 score cp 89 nodes 42 nps 4666 tbhits 0 time 9 pv d2d4 a7a6",
+			fmt.Sprintf("bestmove %s ponder a7a6", bestMove),
+		},
+	}
+
+	e, err := uci.NewEngine(mc.Command, mockEnginePath)
+	assert.NoError(t, err)
+
+	err = e.Run(
+		uci.GoCommand(),
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, bestMove, e.Results.BestMove)
+
+	err = e.Close()
+	assert.NoError(t, err)
+}
 
 func TestIsReadyCommand(t *testing.T) {
 	mc := mockCommander{
