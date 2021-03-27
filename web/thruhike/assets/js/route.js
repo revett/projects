37e00a9df235de28edf2e7438229afba8276360e
revett <2796074+revett@users.noreply.github.com>
2021-03-27 19:39:43 +0000

    Refactor route.js

--- web/thruhike/assets/js/route.js
+++ web/thruhike/assets/js/route.js
@@ -1,52 +1,95 @@
-import mapboxgl from "mapbox-gl";
+import axios from "axios";
+import mapbox from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-mapboxgl.accessToken = "...";
+mapbox.accessToken =
+  "pk.eyJ1IjoicmV2Y2QiLCJhIjoiY2ttcnczcTBnMGMxZTJ3czE4dHFqMXk1ZyJ9.psM5qytFbaYbm_vqOAXUNA";
 
-const coordinates = route.geometry.coordinates;
-const bounds = coordinates.reduce(function (bounds, coord) {
-  return bounds.extend(coord);
-}, new mapboxgl.LngLatBounds(coordinates[0], coordinates[0]));
-
-const map = new mapboxgl.Map({
-  bounds: bounds,
-  container: "map",
-  fitBoundsOptions: {
-    padding: 80,
-  },
-  style: "mapbox://styles/mapbox/outdoors-v11",
-});
-
-map.on("load", function () {
-  map.addSource("route", {
-    type: "geojson",
-    data: route,
-  });
-
-  map.addLayer({
-    id: "route",
-    type: "line",
-    source: "route",
-    layout: {
-      "line-join": "round",
-      "line-cap": "round",
-    },
-    paint: {
-      "line-color": "#1a602f",
-      "line-width": 3,
-    },
-  });
+const addMarkers = (m, d) => {
+  const coordinates = mergeCoordinatesFromFeatures(d);
+  const markerLocs = [coordinates[0], coordinates[coordinates.length - 1]];
 
   const markerOpts = {
     color: "#197e33",
     scale: 0.5,
   };
-  const markerLocs = [
-    route.geometry.coordinates[0],
-    route.geometry.coordinates[route.geometry.coordinates.length - 1],
-  ];
 
   for (const loc of markerLocs) {
-    new mapboxgl.Marker(markerOpts).setLngLat(loc).addTo(map);
+    new mapbox.Marker(markerOpts).setLngLat(loc).addTo(m);
   }
-});
+};
+
+const addRouteLayer = (m, d) => {
+  m.on("load", function () {
+    m.addSource("route", {
+      type: "geojson",
+      data: d,
+    });
+
+    m.addLayer({
+      id: "route",
+      type: "line",
+      source: "route",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-color": "#1a602f",
+        "line-width": 3,
+      },
+    });
+
+    addMarkers(m, d);
+  });
+};
+
+const calculateBounds = (d) => {
+  const coordinates = mergeCoordinatesFromFeatures(d);
+
+  return coordinates.reduce(function (bounds, coord) {
+    return bounds.extend(coord);
+  }, new mapbox.LngLatBounds(coordinates[0], coordinates[0]));
+};
+
+const initialiseMap = (d) => {
+  return new mapbox.Map({
+    bounds: calculateBounds(d),
+    container: "map",
+    fitBoundsOptions: {
+      padding: 80,
+    },
+    style: "mapbox://styles/mapbox/outdoors-v11",
+  });
+};
+
+const mergeCoordinatesFromFeatures = (d) => {
+  let c = [];
+
+  for (const f of d.features) {
+    c.push(...f.geometry.coordinates);
+  }
+
+  return c;
+};
+
+const parseLastPathPart = () => {
+  const p = window.location.pathname.split("/");
+  return p.pop() || p.pop();
+};
+
+const renderRoute = () => {
+  const routeName = parseLastPathPart();
+
+  axios
+    .get(`/data/${routeName}.json`)
+    .then((r) => {
+      const map = initialiseMap(r.data);
+      addRouteLayer(map, r.data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+renderRoute();
