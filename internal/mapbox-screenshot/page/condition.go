ba1588573b9414410fd2e84ca5550089651bcb07
revett <2796074+revett@users.noreply.github.com>
2021-04-20 19:02:55 +0100

    New condition logic for checking via JS

--- internal/mapbox-screenshot/page/condition.go
+++ internal/mapbox-screenshot/page/condition.go
@@ -1,16 +1,28 @@
 package page
 
-import "github.com/tebeka/selenium"
+import (
+	"fmt"
+
+	"github.com/tebeka/selenium"
+)
+
+const localStorageKey = "mapbox.StylesLoaded"
 
 // ElementExistsCondition returns a selenium.Condition which checks if a web
 // element exists using a CSS selector.
 func ElementExistsCondition(s string) selenium.Condition {
 	return func(wd selenium.WebDriver) (bool, error) {
-		om := New(wd)
-		if _, err := om.FindElement(s); err != nil {
+		s := fmt.Sprintf("return localStorage.getItem(\"%s\")", localStorageKey)
+
+		v, err := wd.ExecuteScript(s, nil)
+		if err != nil {
 			return false, err
 		}
 
-		return true, nil
+		if v == "true" {
+			return true, nil
+		}
+
+		return false, nil
 	}
 }
